32f23051c3b8dabeb5d2e3a3fdb97bb83b610537
桑村直弥 <kuwamuranaoya@mbp.local>
2019-01-11 13:20:38 +0900

    Initial Commit

---
+++ SchoolCreditCheckMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SchoolCreditCheckMac
+//
+//  Created by 桑村直弥 on 2019/01/11.
+//  Copyright © 2019年 noy4. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
